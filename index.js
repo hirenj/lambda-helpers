688053a8c6f396adfea70d239d062b351943f61b
Hiren Joshi <hirenj@gmail.com>
2016-05-16 21:31:38 +0200

    Add get_file to helpers

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 "use strict";
 
+const https = require('https');
+
 exports.AWS = require('./aws-promise');
 exports.events = require('./events');
+
+exports.get_file = function(url) {
+	return new Promise(function(resolve,reject) {
+		https.get(url, function(res) {
+			res.setEncoding('utf8');
+			let body = '';
+			res.on('data', (chunk) => body += chunk);
+			res.on('end', function() {
+				body = JSON.parse(body);
+				resolve(body);
+			});
+		}).on('error',function(err) {
+			reject(err);
+		});
+	});
+};
